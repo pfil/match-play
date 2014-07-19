a528ea0afe63758fc73ea6c1e6354b578929a20d
pfil <pfil@Philip-Broerings-MacBook-Pro.local>
2014-07-18 21:31:13 -0500

    adding orchestrate

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var db = require("orchestrate")("e462ea01-a628-4d3d-8e51-74597885f32d")
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+  db.get('matches','1') {
+    .then(function (result) {
+      res.send('Success');
+    })
+    .fail(function (err) {
+      res.send('Error');
+    })
+  }
+
+
 });
 
 var port = Number(process.env.PORT || 5000);
